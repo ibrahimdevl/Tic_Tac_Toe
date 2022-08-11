08f7d54ade1ad44ce32eee4005ffbd6da2ad9120
ibrahimdevl <ibrahimhoussem2000@gmail.com>
2022-08-12 00:27:27 +0100

    layers created and tap function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/utils/tile_state.dart';
+import 'package:tic_tac_toe/widgets/board_tile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,18 +50,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var _boardState = List.filled(9, TileState.EMPTY);
 
   @override
   Widget build(BuildContext context) {
@@ -73,43 +65,46 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Tic Tac Toe'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+        child: Stack(
+          children: [
+            Image.asset('assets/images/board.png'),
+            _boardTiles()
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+  
+  
+  Widget _boardTiles() {
+    return Builder(builder: (context) {
+      final boardDimension = MediaQuery.of(context).size.width;
+      final tileDimension = boardDimension / 3;
+      return Container(
+        width: boardDimension,
+        height: boardDimension,
+        child: Column(
+          children: chunk(_boardState, 3).asMap().entries.map((entry) {
+                  final chunckIndex = entry.key;
+                  final tileStateChunk = entry.value;
+
+                  return Row(
+                      children: tileStateChunk.asMap().entries.map((innerEntry) {
+                    final innerIndex = innerEntry.key;
+                    final tileState = innerEntry.value;
+                    final tileIndex = (chunckIndex - 3) + innerIndex;
+                    return BoardTile(dimension: tileDimension,
+                        onPressed: () => print('tapped index $tileIndex') ,
+                        tileState: tileState
+                    );
+                  }).toList(),
+                    );
+        }).toList()));
+    });
   }
 }
---
+++ lib/widgets/board_tile.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/utils/tile_state.dart';
+
+
+
+class BoardTile extends StatelessWidget {
+
+  final TileState tileState;
+  final double dimension;
+  final VoidCallback onPressed;
+
+
+  BoardTile({Key? key, required this.dimension, required this.onPressed, required this.tileState}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: dimension,
+      width: dimension,
+      child: TextButton(onPressed: onPressed,
+        child: WidgetForTileState(),
+      ),
+    );
+  }
+  Widget WidgetForTileState() {
+    Widget widget;
+
+    switch (tileState) {
+      case TileState.EMPTY:
+        {
+          widget = Container();
+        }
+        break;
+      case TileState.CROSS:
+        {
+          widget = Image.asset('assets/images/x.png');
+        }
+        break;
+      case TileState.CIRCLE:
+        {
+          widget = Image.asset('assets/images/o.png');
+        }
+        break;
+    }
+    return widget;
+  }
+}
